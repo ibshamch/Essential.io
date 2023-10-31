1a1d23944bdd1ef36708d5b12723b4985ba64885
Ibsham Chaudhary <progwithibi@gmail.com>
2023-10-31 09:21:11 +0500

    header made

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <div className="App">Essential.io</div>;
-}
+const App = () => {
+  const navLinks = ["Product", "Resource", "Our Work"];
+  return (
+    <div className="App">
+      <header className="header">
+        <div className="favicon">
+          <div>Favicon</div>
+          <h3>/hello@essential</h3>
+        </div>
+        <div className="navigation-list">
+          {navLinks.map((link) => {
+            return <div>{link}</div>;
+          })}
+        </div>
+        <div className="header-buttons">
+          <h3>FAQ</h3>
+          <button className="header-btn-download">Download API</button>
+          <div>Menu Bar</div>
+        </div>
+      </header>
+    </div>
+  );
+};
 
 export default App;
