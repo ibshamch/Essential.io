4a8267839a7acbfe0a7ec017bd18161104effd5d
Ibsham Chaudhary <progwithibi@gmail.com>
2023-10-31 09:31:29 +0500

    Header Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
+import Header from "./Components/header/header.component";
 const App = () => {
   const navLinks = ["Product", "Resource", "Our Work"];
   return (
     <div className="App">
-      <header className="header">
-        <div className="favicon">
-          <div>Favicon</div>
-          <h3>/hello@essential</h3>
-        </div>
-        <div className="navigation-list">
-          {navLinks.map((link) => {
-            return <div>{link}</div>;
-          })}
-        </div>
-        <div className="header-buttons">
-          <h3>FAQ</h3>
-          <button className="header-btn-download">Download API</button>
-          <div>Menu Bar</div>
-        </div>
-      </header>
+      <Header navLinks={navLinks} />
     </div>
   );
 };
---
+++ src/Components/header/header.component.jsx
@@ -0,0 +1,24 @@
+import { ReactComponent as Favicon } from "../../assets/logo-waveform-svgrepo-com.svg";
+import { ReactComponent as MenuBar } from "../../assets/menu-bottom-left-svgrepo-com.svg";
+const Header = ({ navLinks }) => {
+  return (
+    <header className="header">
+      <div className="favicon">
+        <Favicon className="favicon" />
+        <h3>/hello@essential</h3>
+      </div>
+      <div className="navigation-list">
+        {navLinks.map((link) => {
+          return <div>{link}</div>;
+        })}
+      </div>
+      <div className="header-buttons">
+        <h3>FAQ</h3>
+        <button className="header-btn-download">Download API</button>
+        <MenuBar className="menu-bar" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
